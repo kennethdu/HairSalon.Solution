a547c42c2882c280d18115a399edfb9f6b7f7ea0
Kenneth Du <kennethdu3@gmail.com>
2018-09-21 08:41:43 -0700

    add Save methods to Client.cs and Employee.cs files

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -10,16 +10,38 @@ namespace HairSalon.Models
         private string _clientName;
         private int _clientId;
         private int _employeeId;
-        public Client(string newClient, int EmployeeId, int _clientId = 0)
+        public Client(string newClient, int EmployeeId, int ClientId = 0)
         {
             _clientName = newClient;
             _employeeId = EmployeeId;
+            _clientId = ClientId;
         }
-        public int GetClientId()
+        public string GetClient()
         {
-            return _clientId;
+            return _clientName;
         }
-        
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"INSERT INTO client (name) VALUES (@name);";
+
+            MySqlParameter name = new MySqlParameter();
+            name.ParameterName = "@name";
+            name.Value = this._clientName;
+            cmd.Parameters.Add(name);
+
+            cmd.ExecuteNonQuery();
+            _clientId = (int)cmd.LastInsertedId;
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
 
     }
 }
--- HairSalon/Models/Employee.cs
+++ HairSalon/Models/Employee.cs
@@ -12,7 +12,35 @@ namespace HairSalon.Models
         public Employee(string EmployeeName, int EmployeeId = 0)
         {
             _employeeName = EmployeeName;
+            _employeeId = EmployeeId;
         }
+        private string GetEmployee()
+        {
+            return _employeeName;
+        }
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = @"INSERT INTO employee (name) VALUES (@name);";
+
+            MySqlParameter name = new MySqlParameter();
+            name.ParameterName = "@name";
+            name.Value = this._employeeName;
+            cmd.Parameters.Add(name);
+
+            cmd.ExecuteNonQuery();
+            _employeeId = (int)cmd.LastInsertedId;
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
 
     }
 }
