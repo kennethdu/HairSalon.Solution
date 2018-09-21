e93e91fa811ae5450642aa58494e2830b7619492
Kenneth Du <kennethdu3@gmail.com>
2018-09-21 11:59:53 -0700

    update ClientController.cs file

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -6,11 +6,28 @@ namespace HairSalon.Controllers
 {
     public class ClientController : Controller
     {
-        [HttpGet("/")]
+        [HttpGet("/allclients")]
         public ActionResult Index()
         {
             List<Client> allClients = Client.GetAllClient();
             return View(allClients);
+        }
+        [HttpGet("/employee/{employeeId}/client/new")]
+        public ActionResult CreateForm(int employeeId)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Employee employee = Employee.Find(employeeId);
+            return View(employee);
+        }
+        [HttpGet("/employee/{employeeId}/client/{clientId}")]
+        public ActionResult Details(int employeeId, int clientId)
+        {
+            Client client = Client.Find(clientId);
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Employee employee = Employee.Find(employeeId);
+            model.Add("client", client);
+            model.Add("employee", employee);
+            return View(model);
         }
 
     }
